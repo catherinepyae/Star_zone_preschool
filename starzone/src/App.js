e48da1774c372dc06d360d4950198f8cd71a3d9e
Pyae Phyo Thet <catherinepyae1296@gmail.com>
2023-06-14 13:23:44 +0630

    navbar added

--- starzone/src/App.js
+++ starzone/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import {Footer,Header,Blog,AboutStarZone,Features,Classes} from './containers'
-import {CTA,Brand,Navbar} from './components';
+import {CTA,Brand,Navba} from './components';
+import './App.css';
+
+ 
 
 //react functional component
 const App = () => {
   return (
     <div className='App'>
       <div className='school_bg'>
-        <Navbar/>
+        <Navba/>
         <Header/>
       </div>
       <Brand/>
